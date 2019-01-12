ec314d7a943913f8fe20c9682b833dd936d627bb
artinmajdi <msm2024@gmail.com>
2019-01-11 23:34:33 -0700

    s

--- otherFuncs/Extra_mergingResults.py
+++ otherFuncs/Extra_mergingResults.py
@@ -1,28 +1,46 @@
 
 import os, sys
-__file__ = '/array/ssd/msmajdi/code/thalamus/keras/'
-sys.path.append(os.path.dirname(__file__))
+# __file__ = '/array/ssd/msmajdi/code/thalamus/keras/'
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 import numpy as np
+import pandas as pd
+from otherFuncs import smallFuncs
 from Parameters import UserInfo, paramFunc
 
-params = paramFunc.Run(UserInfo.__dict__)
+def merginResults(Dir):
+        subF = os.listdir(Dir)
+        subF = [a for a in subF if 'vimp' in a]
+        Dice_Test = []
 
+        _, FullIndexes = smallFuncs.NucleiSelection(1)
+        names = np.append(['subjects'], smallFuncs.AllNucleiNames(FullIndexes))
+
+        for subject in subF:
+                Dir_subject = Dir + '/' + subject
+                a = os.listdir(Dir_subject)
+                a = [i for i in a if 'Dice_' in i]
+
+                Dice_Single = list(np.zeros(len(FullIndexes)+1))
+                Dice_Single[0] = subject
+                for n in a:
+                        b = np.loadtxt(Dir_subject + '/' + n)
+                        index = int(b[0])
+                        if index != 4567: 
+                                Dice_Single[index] = b[1] 
+                        else: 
+                                Dice_Single[3] = b[1] 
+                                
+                Dice_Test.append(Dice_Single)
+
+        df = pd.DataFrame(data=Dice_Test, columns=names)
+        df.to_csv(Dir + '/Dices.csv', index=False)
+
+        return Dice_Test
+
+# params = paramFunc.Run(UserInfo.__dict__)
 Dir = params.directories.Test.Result
-subF = os.listdir(Dir)
-
-Dice_Test = (np.zeros((len(subF), 14)))
-for ind, subject in enumerate(subF):
-    Dir_subject = Dir + '/' + subject
-    a = os.listdir(Dir_subject)
-    a = [i for i in a if 'Dice_' in i]
-
-    for n in a:
-        b = np.loadtxt(Dir_subject + '/' + n)
-        Dice_Test[ ind, int(b[0])] = b[1]
 
 
+Dir = '/media/data1/artin/a/subExp1_Loss_Dice'
 
-
-print('----------')
-print(Dice_Test)
-
+merginResults(Dir)
