a5e06b9dc7fbd044a31facd048988ed85b2aa5f2
artinmajdi <msm2024@gmail.com>
2019-05-12 16:03:26 -0700

    fds

---
+++ otherFuncs/Extra_saveMiddleSlice.py
@@ -0,0 +1,59 @@
+# import numpy as np
+import os, sys
+# sys.path.append(os.path.dirname(__file__))
+# sys.path.append('/array/ssd/msmajdi/code/thalamus/keras')
+import nibabel as nib
+import numpy as np
+import matplotlib.pyplot as plt
+import imageio
+import skimage
+import otherFuncs.smallFuncs as smallFuncs
+# import Parameters.UserInfo as UserInfo
+# import Parameters.paramFunc as paramFunc
+# params = paramFunc.Run(UserInfo.__dict__, terminal=True)
+
+class Input_cls():
+    def __init__(self, dir_in='' , dir_out=''):
+
+        def directories(self):
+            for en in range(len(sys.argv)):
+
+                if sys.argv[en].lower() in ('-i','--input'):    dir_in = sys.argv[en+1]
+                elif sys.argv[en].lower() in ('-o','--output'): dir_out = sys.argv[en+1]
+            self.dir_in, self.dir_out = dir_in , dir_out
+                           
+        directories(self)
+
+        self.subjList = [s for s  in os.listdir(self.dir_in) if 'vimp' in s]
+
+        
+    def save_middleSlice_per_subject(self,subj):
+        im = nib.load(self.dir_in + '/' + subj + '/WMnMPRAGE_bias_corr.nii.gz')
+        imm = im.get_data()[...,int(im.shape[2]/2)]
+
+        imm2 = skimage.transform.rotate(imm,90,resize=True)
+        imageio.imwrite(self.dir_out + '/' + subj + '.jpg', imm2)  
+
+
+input = Input_cls()
+
+for subj in input.subjList:
+    input.save_middleSlice_per_subject(subj)
+
+
+
+
+# subj = 'vimp2_901_07052013_AS_MS'
+# dir_in = '/home/artinl/Documents/RESEARCH/dataset/7T/' + subj + '/WMnMPRAGE_bias_corr.nii.gz'
+# im = nib.load(dir_in)
+
+# imm = im.get_data()[...,int(im.shape[2]/2)]
+
+# plt.imshow(imm,cmap='gray')
+
+
+# dir_out = '/home/artinl/Documents/RESEARCH/'
+
+# imm2 = skimage.transform.rotate(imm,90,resize=True)
+# imageio.imwrite(dir_out + subj + '.jpg', imm2)
+
--- otherFuncs/test.py
+++ otherFuncs/test.py
@@ -6,7 +6,7 @@ import numpy as np
 import Parameters.UserInfo as UserInfo
 import Parameters.paramFunc as paramFunc
 
-params = paramFunc.Run(UserInfoB, terminal=True)
+params = paramFunc.Run(UserInfo.__dict__, terminal=True)
 
 dir = 'dir to results'
 a = [s for s in os.listdir(dir + 'sd0/') if 'vimp' in s]
@@ -15,32 +15,32 @@ c = [s for s in os.listdir(dir + 'sd2/') if 'vimp' in s]
 subjectList = list(set(a+b+c))
 
 
-def load_msks_Allsd(dir + sd + subj + '/' +  label):
-    def readAllsd(sd):
-        msk = nib.load(dir + sd + subj + '/' +  label + '.nii.gz')
-        return msk.get_data().reshape(-1).transpose()
+# def load_msks_Allsd(dir + sd + subj + '/' +  label):
+#     def readAllsd(sd):
+#         msk = nib.load(dir + sd + subj + '/' +  label + '.nii.gz')
+#         return msk.get_data().reshape(-1).transpose()
 
-    X = [readAllsd(sd) for sd in ['sd0/' , 'sd1/' , 'sd2/'] ]
+#     X = [readAllsd(sd) for sd in ['sd0/' , 'sd1/' , 'sd2/'] ]
     
 
 
-for subj in params.directories.Input.subjects:
+# for subj in params.directories.Input.subjects:
     
-    clf = tree.DecisionTreeClassifier()
-    clf = clf.fit(X,Y)
+#     clf = tree.DecisionTreeClassifier()
+#     clf = clf.fit(X,Y)
 
 
 
-dir = '/home/artinl/Documents/RESEARCH/dataset/Main/vimp2_819_05172013_DS_MS/Label/1-THALAMUS_PProcessed.nii.gz'
-msk = nib.load(dir)
+# dir = '/home/artinl/Documents/RESEARCH/dataset/Main/vimp2_819_05172013_DS_MS/Label/1-THALAMUS_PProcessed.nii.gz'
+# msk = nib.load(dir)
 
-a.shape
+# a.shape
 
-X = []
-X.append(a)
-X
-b = a.reshape(msk.shape)
-b.shape
+# X = []
+# X.append(a)
+# X
+# b = a.reshape(msk.shape)
+# b.shape
 
-np.unique(msk.get_data())
-clf.predict(X2)
+# np.unique(msk.get_data())
+# clf.predict(X2)
