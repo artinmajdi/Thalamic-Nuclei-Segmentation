f14a1fd9f50f5413b19270889ea31e394aba3ed3
artinmajdi <msm2024@gmail.com>
2019-11-28 13:16:51 -0700

    hgf

--- otherFuncs/Extra_drawing_precision_recall_curve.py
+++ otherFuncs/Extra_drawing_precision_recall_curve.py
@@ -8,12 +8,12 @@ import numpy as np
 import nibabel as nib
 import matplotlib.pyplot as plt
 
-dir = '/array/ssd/msmajdi/experiments/keras/exp6/results/sE13_Cascade_FM20_ResFCN_ResUnet2_TL_NL3_LS_MyLogDice_US1_FCNA0_FCNB0_FM0_permute0_CSFn2_TL_Main_CV_a_for_percision_recall_curve/sd2/vimp2_A_CSFn2/'
-dirM = '/array/ssd/msmajdi/experiments/keras/exp6/crossVal/CSFn2/a/vimp2_A_CSFn2/Label/'
+dir = '/array/ssd/msmajdi/experiments/keras/exp7/results/sE12_Cascade_FM00_Res_Unet2_NL3_LS_MyLogDice_US1_wLRScheduler_Main_Ps_ET_Init_3T_CV_a/2.5D_MV/vimp2_988_wmn/'
+dirM = '/array/ssd/msmajdi/experiments/keras/exp7/crossVal/temp/Main/b/vimp2_988_08302013_CB/Label/'
 a = smallFuncs.Nuclei_Class(index=1,method='Cascade').All_Nuclei()
 
-msk = nib.load(dir + a.Names[0] + '.nii.gz').get_data()
-mskM = nib.load(dirM + a.Names[0] + '.nii.gz').get_data()
+msk = nib.load(dir  + '8-Pul.nii.gz').get_data()
+mskM = nib.load(dirM  + '8-Pul_PProcessed.nii.gz').get_data()
 
 
 plt.plot(np.unique(msk))
